f414a484f003233a3abf7bf84c10bbd1c545b69d
Brody <brodypen7987@gmail.com>
2024-09-22 06:57:04 -0500

    feat: push3

--- src/api/reminders/use-reminders.ts
+++ src/api/reminders/use-reminders.ts
@@ -2,10 +2,10 @@ import type { AxiosError } from 'axios';
 import { createQuery } from 'react-query-kit';
 
 import { client } from '../common';
-import type { Course } from './types';
+import type { Reminder} from './types';
 
 type Variables = { id: string };
-type Response = Course[];
+type Response = Reminder[];
 
 export const useReminders = createQuery<Response, Variables, AxiosError>({
   queryKey: ['reminders'],
